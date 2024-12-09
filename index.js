2419643e164a9d28681ce23b97d18ddcdabfb69f
cornsnake <cornsnenk@gmail.com>
2024-12-09 14:09:11 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express"),
-    router = express();
+    router = express(),
+    utils = require("./mw");
 
-router.get("/", (r,s)=>s.send(`Bienvenue.\nHoy es: ${(new Date()).toISOString()}`));
+router.get("/first/", (r,s)=>s.send(utils.time_string(1)));
+router.get("/second/", (r,s)=>s.send(utils.time_string(2)));
 
 router.listen(8080);
