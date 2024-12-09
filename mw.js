da9b11508f7ec5701293b7505d0ba3c41097ba48
corn-snake <cornsnenk@gmail.com>
2024-12-09 14:22:00 +0000

    oops

--- mw.js
+++ mw.js
@@ -1,13 +1,16 @@
+
+const time_array= ()=>{
+  const d = new Date();
+  return [d.getHours(), d.getMinutes(), d.getDate(), d.getMonth(), d.getFullYear()];
+},
+month_array= ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre", "Numa"],
+zero_index_month_array= [0, ...month_array],
+month_name= (m, bias=false)=>bias ? zero_index_month_array[m] : month_array[m],
+time_string= (r)=>{
+  const d = time_array();
+  return `Esta es la ruta #${r}. Accediste el día ${d[2]} de ${month_name(d[3])} del ${d[4]}, a las ${d[0]}:${d[1]}`;
+};
+
 module.exports = {
-  time_array: ()=>{
-    const d = new Date();
-    return [d.getHours(), d.getMinutes(), d.getDate(), d.getMonth(), d.getYear()];
-  },
-  month_array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre", "Numa"],
-  zero_index_month_array: [0, ...this.month_array],
-  month_name: (m, bias=false)=>bias ? this.zero_index_month_array[m] : this.month_array[m],
-  time_string: (r)=>{
-    const d = this.time_array();
-    return `Esta es la ruta #${r}. Accediste el día ${d[2]} de ${month_name(d[3])} del ${d[4]}, a las ${d[0]}:${d[1]}`;
-  }
+  time_array, month_array, zero_index_month_array, month_name, time_string
 };
--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express = require("express"),
     router = express(),
     utils = require("./mw");
 
+router.get("/", (r,s)=>s.send(`<a href="first">primera ruta</a><br><a href="second">segunda ruta</a>`));
 router.get("/first/", (r,s)=>s.send(utils.time_string(1)));
 router.get("/second/", (r,s)=>s.send(utils.time_string(2)));
 
