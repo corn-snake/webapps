582d20b917eff2bc0a848ad57a493385645230b3
corn-snake <cornsnenk@gmail.com>
2025-02-01 07:06:42 +0000

    catch bad resolution

--- api/branches/edit.js
+++ api/branches/edit.js
@@ -3,7 +3,7 @@ import { _editUser } from "../db/roots/edit.js";
 
 const edit = new Router()
     .patch("/:uid", async ctx=>{
-        ctx.response.body = await _editUser(ctx.params.uid, await ctx.request.body.json()).then(() =>"Usuario actualizado correctamente");
+        ctx.response.body = await _editUser(ctx.params.uid, await ctx.request.body.json()).then(() => "Usuario actualizado correctamente").catch(e => e.errorResponse.errmsg);
     });
 
 export default edit;
